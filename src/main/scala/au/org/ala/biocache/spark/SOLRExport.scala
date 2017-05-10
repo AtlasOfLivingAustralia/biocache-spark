d303620cb9a24d3720b35abe8571dbea79be1333
timrobertson100 <timrobertson100@gmail.com>
2017-05-10 17:04:13 +0200

    Adds safe as CSV per group example

--- src/main/scala/au/org/ala/biocache/spark/SOLRExport.scala
+++ src/main/scala/au/org/ala/biocache/spark/SOLRExport.scala
@@ -9,6 +9,9 @@ import org.apache.spark.sql.SQLContext
   * This is a test to evaluate the feasibility of exporting from SOLR using Spark.
   */
 object SOLRExport {
+  val fields = List("family", "basis_of_record")
+
+
     def main(args: Array[String]) {
       val conf = new SparkConf().setAppName("Spark SOLR example")
       conf.setIfMissing("spark.master", "local[2]")
@@ -20,13 +23,22 @@ object SOLRExport {
         val occurrences = sqlContext.read.format("solr").options(
           Map("zkHost" -> "35.157.64.9", "collection" -> "biocache")
         ).load
-          .filter("data_resource_uid='dr819'")
+          .filter("data_resource_uid='dr346'")
 
         occurrences.printSchema()
         occurrences.show()
         occurrences.registerTempTable("occurrence")
         sqlContext.sql("SELECT family,count(*) AS cnt FROM occurrence GROUP BY family ORDER BY cnt DESC").show()
 
+        // The following is a workaround to circumvent the fact that the CSV writers in Spark 1.6 are not partitionable.
+        // We save as a parquet file per family, and then read each parquet file and safe it as a CSV.
+        occurrences.write.partitionBy("family").format("parquet").save("/tmp/export/transient/")
+        sqlContext.sql("SELECT distinct family FROM occurrence").collect().map(family => {
+          val familyName = family.getString(0)
+          println("Opening " + familyName)
+          val df = sqlContext.read.format("parquet").load("/tmp/export/transient/family=" + familyName)
+          df.write.format("com.databricks.spark.csv").save("/tmp/export/" + familyName)
+        })
 
       } finally {
         sc.stop()
