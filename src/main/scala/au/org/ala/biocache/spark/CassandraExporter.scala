6886b394f310e57a23eafe649bd4d1e9fbe34ee5
Dave Martin <djtfmartin@gmail.com>
2017-05-15 16:06:06 +0100

    work in progress

--- src/main/scala/au/org/ala/biocache/spark/CassandraExporter.scala
+++ src/main/scala/au/org/ala/biocache/spark/CassandraExporter.scala
@@ -10,7 +10,7 @@ import org.apache.spark.sql.cassandra._
   */
 object CassandraExporter {
 
-  val fields = List("dataresourceuid","uuid", "catalogNumber", "collectionCode", "institutionCode", "scientificName_p", "recordedBy",
+  val fields = List("dataresourceuid", "uuid", "catalogNumber", "collectionCode", "institutionCode", "scientificName_p", "recordedBy",
     "taxonConceptID_p",
     "taxonRank_p", "kingdom_p", "phylum_p", "classs_p", "order_p", "family_p", "genus_p", "species_p"
     ,
@@ -46,18 +46,55 @@ object CassandraExporter {
         }
       )
 
-      val exportQuery = "SELECT " + fields.map("first(clean(" + _.toLowerCase + "))").mkString(",") + " FROM occ GROUP BY dataresourceuid"
+      val exportQuery = "SELECT " + fields.map(field => "first(clean(" + field.toLowerCase + ")) AS " + field.toLowerCase).mkString(",") + " FROM occ"
 
-      sqlContext.sql(exportQuery).show()
+      // Write a single parquet file of cleaned data per data resource
+      sqlContext.sql(exportQuery).write.partitionBy("dataresourceuid").format("parquet").save("/data/biocache-exports/transient/")
 
       // The following is a workaround to circumvent the fact that the CSV writers in Spark 1.6 are not partitionable.
-      // We save as a parquet file per family, and then read each parquet file and safe it as a CSV.
-      df.write.partitionBy("dataresourceuid").format("parquet").save("/data/biocache-exports/transient/")
-      sqlContext.sql(exportQuery).collect().map(dataresourceuid => {
+
+      // get the dataresource keys
+      val resourceUids = sqlContext.sql("SELECT distinct dataresourceuid FROM occ").collect()
+
+      // for each resource, read the parquet file and write a CSV
+      resourceUids.map(dataresourceuid => {
         val drUid = dataresourceuid.getString(0)
         val df = sqlContext.read.format("parquet").load("/data/biocache-exports/transient/dataresourceuid=" + drUid)
         df.write.format("com.databricks.spark.csv").save("/data/biocache-exports/" + drUid)
       })
+
+
+//      val exportQuery = "SELECT " + fields.map(field => "first(clean(" + field.toLowerCase + ")) AS " + field.toLowerCase ).mkString(",") + " FROM occ"
+//
+//      // Write a single parquet file of cleaned data per data resource
+//      sqlContext.sql(exportQuery).write.partitionBy("dataresourceuid").format("parquet").save("/data/biocache-exports/transient/")
+//
+//      // The following is a workaround to circumvent the fact that the CSV writers in Spark 1.6 are not partitionable.
+//
+//      // get the dataresource keys
+//      val resourceUids = sqlContext.sql("SELECT distinct dataresourceuid FROM occ").collect()
+//
+//      // for each resource, read the parquet file and write a CSV
+//      resourceUids.map(dataresourceuid => {
+//        val drUid = dataresourceuid.getString(0)
+//        val df = sqlContext.read.format("parquet").load("/data/biocache-exports/transient/dataresourceuid=" + drUid)
+//        df.write.format("com.databricks.spark.csv").save("/data/biocache-exports/" + drUid)
+//      })
+
+//      val exportQuery = "SELECT first(dataresourceuid), " + fields.map("first(clean(" + _.toLowerCase + "))").mkString(",") +
+//        " FROM occ"
+//
+//      val exportQuery = "SELECT " + fields.map("first(clean(" + _.toLowerCase + "))  " + _.toLowerCase).mkString(",") + " FROM occ"
+//
+//
+//      // The following is a workaround to circumvent the fact that the CSV writers in Spark 1.6 are not partitionable.
+//      // We save as a parquet file per family, and then read each parquet file and safe it as a CSV.
+//      sqlContext.sql(exportQuery).write.partitionBy("dataresourceuid").format("parquet").save("/data/biocache-exports/transient/")
+//      sqlContext.sql("SELECT distinct dataresourceuid FROM occ").collect().map(dataresourceuid => {
+//        val drUid = dataresourceuid.getString(0)
+//        val df = sqlContext.read.format("parquet").load("/data/biocache-exports/transient/dataresourceuid=" + drUid)
+//        df.write.format("com.databricks.spark.csv").save("/data/biocache-exports/" + drUid)
+//      })
 
     } finally {
       sc.stop()
